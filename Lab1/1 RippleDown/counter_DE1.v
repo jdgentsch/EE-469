f0f2f28e3eb7d2e35081fd2a68b25f86a1dd7c5f
Jack Gentsch <jdgentsch@comcast.net>
2016-04-03 16:21:30 -0700

    SigTap Counter 1
    Took gtkwave and signal tap screenshots of counter 1, can be
    successfully programmed.

--- Lab1/1 RippleDown/counter_DE1.v
+++ Lab1/1 RippleDown/counter_DE1.v
@@ -1,28 +1,28 @@
-// A four-stage ripple up counter with active low reset- Counter 1 of EE 371 Lab 1
+// A four-stage ripple up counter with active low reset- Counter 1 of EE 469 Lab 1
 // Note that this upper level module is intended 
 // for use with the DE1_SoC and Spinal Tap.
 
-// Written by Jack Gentsch and Jacky Wang
-// 10/4/2015 instructed by Professor Peckol
+// Written by Jack Gentsch, Jacky Wang, and Chinh Bui
+// 4/3/2016 instructed by Professor Peckol
 
 //Declaration of DE1_SoC inputs and outputs: LED as output,
 //an active-low reset on Switch 0 and a 50 MHz clock
 module counter_DE1(LEDR, CLOCK_50, SW);
 	//Declaration of clocks, switches, and LEDs for usage
 	output [9:0] LEDR;
-	input [3:0] SW;
+	input [9:0] SW;
 	input CLOCK_50;
 	wire [31:0] clk;
 	
 	//A 50 MHz clock is used for the DE1_SoC and Signal Tap
 	//Testing live on the DE1-SoC uses a value of "25" to create 0.75 Hz
-	parameter clkBit = 25;
+	parameter clkBit = 0;
 	
 	//Set unused LED's to low
 	assign LEDR[9:4] = 0;
 	
 	//Declare an instance of the synthesized schematic module
-	rippleDownCounter myRippleDown (.out(LEDR[3:0]), .clk(clk[clkBit]), .rst(SW[0]));
+	rippleDownCounter myRippleDown (.out(LEDR[3:0]), .clk(clk[clkBit]), .rst(SW[9]));
 
 	//Creates a clock divider to allow for a slower clock
 	clockDiv clkDiv(.clkIn(CLOCK_50), .clkOut(clk));
--- Lab1/1 RippleDown/rippleDownCounter.v
+++ Lab1/1 RippleDown/rippleDownCounter.v
@@ -1,8 +1,8 @@
-// A four-stage ripple up counter with active low reset - Counter 1 of EE 371 Lab 1
+// A four-stage ripple up counter with active low reset - Counter 1 of EE 469 Lab 1
 // Note that this lower level module is the gate level model of the counter.
 
-// Written by Jack Gentsch and Jacky Wang
-// 10/4/2015 instructed by Professor Peckol
+// Written by Jack Gentsch, Jacky Wang, and Chinh Bui
+// 4/3/2016 instructed by Professor Peckol
 
 module rippleDownCounter(out[3:0], clk, rst);
 
@@ -13,9 +13,9 @@ module rippleDownCounter(out[3:0], clk, rst);
 
 	//Construction of flip-flops. Connected to create an asynchronous up counter.
 	DFlipFlop ff0 (.q(out[0]), .qBar(flip[0]), .D(flip[0]), .clk(clk), .rst(rst));
-	DFlipFlop ff1 (.q(out[1]), .qBar(flip[1]), .D(flip[1]), .clk(flip[0]), .rst(rst));
-	DFlipFlop ff2 (.q(out[2]), .qBar(flip[2]), .D(flip[2]), .clk(flip[1]), .rst(rst));
-	DFlipFlop ff3 (.q(out[3]), .qBar(flip[3]), .D(flip[3]), .clk(flip[2]), .rst(rst));
+	DFlipFlop ff1 (.q(out[1]), .qBar(flip[1]), .D(flip[1]), .clk(out[0]), .rst(rst));
+	DFlipFlop ff2 (.q(out[2]), .qBar(flip[2]), .D(flip[2]), .clk(out[1]), .rst(rst));
+	DFlipFlop ff3 (.q(out[3]), .qBar(flip[3]), .D(flip[3]), .clk(out[2]), .rst(rst));
 
 endmodule
 
