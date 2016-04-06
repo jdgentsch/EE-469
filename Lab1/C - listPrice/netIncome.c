df2e332c89af8eeee5191c2ab5260cfe0a56f1cd
chinhbui1989 <chinhbui.vn1989@gmail.com>
2016-04-05 19:05:08 -0700

    Update netIncome.c

--- Lab1/C - listPrice/netIncome.c
+++ Lab1/C - listPrice/netIncome.c
@@ -1,10 +1,9 @@
-
-
 #include <stdio.h>
 
 int main(void)
 {
     float gincome, netincome, taxperc, ssecurity, fedtax, statax;
+
     printf("Please enter gross income:\n");
     scanf("%f", &gincome);
     getchar();
@@ -13,26 +12,33 @@ int main(void)
     scanf("%f", &taxperc);
     getchar();
 
-    if(gincome >= 65000) {
-        ssecurity = 65000*10.3/100;
-    } else {
-        ssecurity = gincome*10.3/100;
+    if (gincome >=0 && taxperc >=0 && taxperc <= 100)
+    {
+        if(gincome >= 65000) {
+            ssecurity = 65000*10.3/100;
+        } else {
+            ssecurity = gincome*10.3/100;
+        }
+
+        if (gincome >= 30000) {
+            fedtax = 3500 + (gincome-30000)*28/100;
+        } else {
+            fedtax = 3500;
+        }
+    } else
+    {
+        printf("Ooops, invalid input range detected... please try again");
+        return 0;
     }
 
-    if (gincome >= 30000) {
-        fedtax = 3500 + (gincome-ssecurity-30000)*28/100;
-    } else {
-        fedtax = 3500;
-    }
-
-    statax = (gincome-ssecurity)*taxperc/100;
+    statax = (gincome)*taxperc/100;
     netincome = gincome - ssecurity - fedtax - statax;
 
-    printf("Your social security due is: %f\n", ssecurity);
-    printf("Your federal income tax is: %f\n", fedtax);
-    printf("Your state tax is: %f\n", statax);
-    printf("Your net income is: %f\n", netincome);
+    printf("Your social security due is: %2.2f\n", ssecurity);
+    printf("Your federal income tax is: %2.2f\n", fedtax);
+    printf("Your state tax is: %2.2f\n", statax);
+    printf("Your net income is: %2.2f\n", netincome);
 
     return 0;
 
-
+}
