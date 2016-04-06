64590d25ea94c6c4bede9d9cf4fe49b73889d6b4
chinhbui1989 <chinhbui.vn1989@gmail.com>
2016-04-05 19:36:32 -0700

    subtract ssecurity from tax

--- Lab1/C - netIncome/netIncome.c
+++ Lab1/C - netIncome/netIncome.c
@@ -21,7 +21,7 @@ int main(void)
         }
 
         if (gincome >= 30000) {
-            fedtax = 3500 + (gincome-30000)*28/100;
+            fedtax = 3500 + (gincome-ssecurity-30000)*28/100;
         } else {
             fedtax = 3500;
         }
@@ -31,7 +31,7 @@ int main(void)
         return 0;
     }
 
-    statax = (gincome)*taxperc/100;
+    statax = (gincome-ssecurity)*taxperc/100;
     netincome = gincome - ssecurity - fedtax - statax;
 
     printf("Your social security due is: %2.2f\n", ssecurity);
