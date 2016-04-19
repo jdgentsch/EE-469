f612decd6aaf26de3834e47619b5bf7cbba14fed
Jacky Wang <tingyu7@uw.edu>
2016-04-18 20:33:41 -0700

    sram update

--- Lab2/sram.v
+++ Lab2/sram.v
@@ -2,20 +2,20 @@
 //Lab 2: SRAM module for our memory subsystem
 //EE 469 with James Peckol 4/8/16
 module sram (data, clk, adrx, nOE, read);
-	inout [15:0] data;
+	inout [31:0] data;
 	input clk;
 	input [10:0] adrx;
 	input nOE; //Active-low output enable
 	input read;
 	
-	wire [15:0] mdrInput;
+	wire [31:0] mdrInput;
 
 	reg [15:0] mem [0:2047];
 	reg [10:0] mar;
-	reg [15:0] mdr;
+	reg [31:0] mdr;
 	
 	//Tristate data lines dependent on output enable signal
-	assign data = ~nOE ? mdr : 16'bz;
+	assign data = ~nOE ? mdr : 32'bz;
 
 	assign mdrInput = read ? mem[mar] : data;
 	
@@ -29,6 +29,6 @@ module sram (data, clk, adrx, nOE, read);
 
 	//Perform the write operation when read signal is strobed high
 	always @(posedge read)
-		mem[mar] <= mdr;
+		mem[mar] <= mdr[15:0];
 
 endmodule
