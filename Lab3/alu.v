ff7f94ecc4ede058b2d891a1e9437ca0f5236d24
Jacky Wang <tingyu7@uw.edu>
2016-04-26 16:40:36 -0700

    ahead start

--- Lab3/alu.v
+++ Lab3/alu.v
@@ -1,13 +1,13 @@
 //Jack Gentsch, Jacky Wang, Chinh Bui
-//Lab 3: ALU
+//Lab 3: ALU with dataflow model
 //EE 469 with James Peckol 4/22/16
 
-module alu (out, zero, overflow, cout, neg, busA, busB, control);
-	output [31:0] out;
-	output zero, overflow, cout, neg;
+module alu (busOut, zero, overflow, Cout, neg, busA, busB, control);
+	output [31:0] busOut;
+	output zero, overflow, Cout, neg;
 	input [31:0] busA, busB;
 	input [2:0] control;
 
-	
+
 
 endmodule
---
+++ Lab3/cla4.v
@@ -0,0 +1,13 @@
+//Jack Gentsch, Jacky Wang, Chinh Bui
+//EE 469, Dr. Peckol 4/15/16
+// 4-bit Carry Look Ahead (cla) Adder
+
+module cla4 (sum, Cout, pOut, gOut, inA, inB, Cin);
+	output [3:0] sum;
+	output Cout, pOut, gOut; // pOut = propagate, gOut = generate
+	input [3:0] inA, inB;
+	input Cin;
+
+
+
+endmodule
