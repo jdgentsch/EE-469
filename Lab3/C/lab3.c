171889e03b3dfeadf63557fc4331460cc0dd4d93
chinhbui1989 <chinhbui.vn1989@gmail.com>
2016-05-01 04:42:49 -0700

    pointer

---
+++ Lab3/C/lab3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A = 25;
+    int B = 16;
+    int C = 7;
+    int D = 4;
+    int E = 9;
+    int result;
+    // assign address of variables to pointers
+    int* A_Ptr = &A;
+    int* B_Ptr = &B;
+    int* C_Ptr = &C;
+    int* D_Ptr = &D;
+    int* E_Ptr = &E;
+    // calculate result using dereference
+    result = ((*A_Ptr - *B_Ptr)*(*C_Ptr + *D_Ptr))/ *E_Ptr;
+    printf("The result is %i\n", result);
+    return 0;
+}
